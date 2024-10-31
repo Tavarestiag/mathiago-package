09232535aaf4ff064189f545d8185247a09278cc
Tiago Alves Tavares <tiagoalvestavares1@gmail.com>
2024-10-31 09:54:01 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="Pacote com funções matemáticas para treino em python",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link",
+    url="https://github.com/Tavarestiag",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
